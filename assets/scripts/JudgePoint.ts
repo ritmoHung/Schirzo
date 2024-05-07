21484278b69199d039c927cac83f234feac105a9
Ritmo Hung <jajh102098@gmail.com>
2024-05-07 22:20:37 +0800

    Feat: JudgePoint events by globalTime

--- assets/scripts/JudgePoint.ts
+++ assets/scripts/JudgePoint.ts
@@ -14,6 +14,7 @@ interface Event {
 export class JudgePoint extends Component {
     private events: any = {}
     private lastGlobalTime: number = -1
+    private lastEventIndexes: { [key: string]: number } = {}
     private lastPropValues: { [key: string]: any } = {}
 
 
@@ -21,75 +22,94 @@ export class JudgePoint extends Component {
     // # Lifecycle
     update(dt: number) {
         const globalTime = ChartPlayer.Instance ? ChartPlayer.Instance.getGlobalTime() : 0;
+
+        // Reset search indexes if time rewinds
+        if (globalTime < this.lastGlobalTime) this.lastEventIndexes = {};
         
         if (globalTime !== this.lastGlobalTime) {
             // Global time changed, update properties
-            this.node.position = this.getPropertyByTime("position", globalTime);
-            this.node.angle = this.getPropertyByTime("rotate", globalTime);
+            this.node.position = this.getPropValueByTime("position", globalTime);
+            this.node.angle = this.getPropValueByTime("rotate", globalTime);
             const uiOpacity = this.node.getComponent(UIOpacity);
-            if (uiOpacity) uiOpacity.opacity = this.getPropertyByTime("opacity", globalTime);
+            if (uiOpacity) uiOpacity.opacity = this.getPropValueByTime("opacity", globalTime);
 
             // Update last global time
             this.lastGlobalTime = globalTime;
         } else {
-            this.node.position = this.lastPropValues["position"];
-            this.node.angle = this.lastPropValues["rotate"];
-            const uiOpacity = this.node.getComponent(UIOpacity);
-            if (uiOpacity) uiOpacity.opacity = this.lastPropValues["opacity"];
+            // this.node.position = this.lastPropValues["position"];
+            // this.node.angle = this.lastPropValues["rotate"];
+            // const uiOpacity = this.node.getComponent(UIOpacity);
+            // if (uiOpacity) uiOpacity.opacity = this.lastPropValues["opacity"];
         }
-
-        // console.log(this.node.position);
     }
 
 
     // # Functions
     initialize(data: any) {
-        console.log(data);
         this.events.speedEvents = data.speedEvents || [];
         this.events.positionEvents = data.positionEvents || [];
         this.events.rotateEvents = data.rotateEvents || [];
         this.events.opacityEvents = data.opacityEvents || [];
     }
 
-    getPropertyByTime(property: string, time: number) {
+    getPropValueByTime(property: string, time: number) {
         const events = this.events[property + "Events"];
-        if (!events || events.length === 0) return;  // TODO: Handle 0 event cases
+        if (!events || events.length === 0) return null;  // Returns null if no events
 
-        // Initialize with first event's start value
+        // ? Case 1: Before the first event
         if (time < events[0].startTime) {
-            return this.extractValue(events[0].start, property);
+            const value = this.extractValue(events[0].start, property);
+            this.lastPropValues[property] = value;
+            return value;
         }
 
-        let lastValue = null;
-        for (let i = 0; i < events.length; i++) {
+        // ? Case 2: After the last event
+        const lastEvent = events[events.length - 1];
+        if (time >= lastEvent.endTime) {
+            const value = this.extractValue(lastEvent.end, property);
+            this.lastPropValues[property] = value;
+            return value;
+        }
+
+        // Search for matching event
+        let value = null;
+        let startIndex = this.lastEventIndexes[property] || 0;
+        for (let i = startIndex; i < events.length; i++) {
             const event = events[i];
 
+            // ? Case 3: During an event
             if (time >= event.startTime && time < event.endTime) {
                 // Ease by event.easing
                 const t = (time - event.startTime) / (event.endTime - event.startTime);
                 const easingFunction = easing[event.easing] || easing.linear;
                 const easedT = easingFunction(t);
 
-                // Extract actual value
+                // Calculate actual value by lerping with eased t
                 const start = this.extractValue(event.start, property);
                 const end = this.extractValue(event.end, property);
-                const value = lerp(start, end, easedT);
-                console.log(`Start: ${JSON.stringify(start)}, End: ${JSON.stringify(end)}, t: ${easedT}, value: ${value}`);
+                if (property === "position") {
+                    value = new Vec3();
+                    Vec3.lerp(value, start, end, easedT);
+                } else {
+                    value = lerp(start, end, easedT);
+                }
+
+                // Save to cache and return
+                this.lastEventIndexes[property] = i;
                 this.lastPropValues[property] = value;
                 return value;
             }
 
-            lastValue = this.extractValue(event.end, property);
-            if (i < events.length - 1 && time >= event.endTime && time < events[i + 1].startTime) {
-                this.lastPropValues[property] = lastValue;
-                return lastValue;
+            // ? Case 4: Between events
+            if (i < events.length - 1 && (time >= event.endTime && time < events[i + 1].startTime)) {
+                value = this.extractValue(event.end, property);
+
+                // Save to cache and return
+                this.lastEventIndexes[property] = i;
+                this.lastPropValues[property] = value;
+                return value;
             }
         }
-
-        const lastEvent = events[events.length - 1];
-        const value = this.extractValue(lastEvent.end, property);
-        this.lastPropValues[property] = value;
-        return value;
     }
 
     extractValue(value: any, property: string) {
