e15ef8191de94e899ed347625a0eacd61a4bb60c
Ritmo Hung <jajh102098@gmail.com>
2024-05-27 17:03:15 +0800

    Implement HoldNote UI

--- assets/scripts/notes/HoldNote.ts
+++ assets/scripts/notes/HoldNote.ts
@@ -1,11 +1,23 @@
-import { _decorator, EventKeyboard } from "cc";
+import { _decorator, EventKeyboard, Node } from "cc";
 import { JudgePoint } from "../JudgePoint";
-import { Note } from './Note';
+import { Note } from "./Note";
 const { ccclass, property } = _decorator;
 
 @ccclass("HoldNote")
 export class HoldNote extends Note {
+    @property(Node)
+    private startSprite: Node
+
+    @property(Node)
+    private endSprite: Node
+
+    @property(Node)
+    private rectSprite: Node
+
     private holdTime: number
+    private startPosition: number
+    private endOffset: number
+    private hasPressed: boolean
 
 
 
@@ -18,8 +30,23 @@ export class HoldNote extends Note {
         // TODO
         const globalTime = this.chartPlayer.getGlobalTime() || 0;
 
-        if (globalTime >= this.time + 0.08) {
-            console.log("MISS");
+        if (globalTime >= this.time && globalTime < this.time + this.holdTime) {
+            if (!this.hasPressed) {
+                this.chartPlayer.playSfx(this.sfx);
+                this.hasPressed = true;
+            }
+
+            const newStartPosition = this.judgePoint.calculatePositionOffset(globalTime);
+
+            // Update StartSprite position
+            const startOffset = newStartPosition - this.startPosition;
+            this.startSprite.setPosition(0, startOffset, 0);
+
+            // Update RectSprite position & scale
+            this.rectSprite.setPosition(0, (startOffset + this.endOffset) / 2, 0);
+            this.rectSprite.setScale(1, this.endOffset - startOffset, 1);
+        } else if (globalTime >= this.time + this.holdTime) {
+            this.chartPlayer.playSfx(this.sfx);
             this.node.destroy();
         }
     }
@@ -31,7 +58,14 @@ export class HoldNote extends Note {
         super.initialize(data, judgePoint);
         this.holdTime = data.holdTime;
 
-        const offset = this.judgePoint.calculatePositionOffset(this.time);
-        this.node.setPosition(0, offset, 0);
+        this.startPosition = this.judgePoint.calculatePositionOffset(this.time);
+        const endPosition = this.judgePoint.calculatePositionOffset(this.time + this.holdTime);
+
+        this.endOffset = endPosition - this.startPosition;
+        this.node.setPosition(0, this.startPosition, 0);
+        this.startSprite.setPosition(0, 0, 0);
+        this.endSprite.setPosition(0, this.endOffset, 0);
+        this.rectSprite.setPosition(0, this.endOffset / 2, 0);
+        this.rectSprite.setScale(1, this.endOffset, 1);
     }
 }
